f4a80dbef9960c4dfa7b3f722ffe99f79ac07235
Jason Cheong <jasonmcheong@gmail.com>
2021-06-22 19:15:50 -0600

    Autto-formatted files

--- src/app/Palindrome.java
+++ src/app/Palindrome.java
@@ -12,129 +12,135 @@ import java.util.ArrayList;
 
 public class Palindrome {
 	/**
-     * Main method that creates a new instance of the Palindrome class and calls the main method
-     * to find the nearest palindrome from a given string of integers
-     * @param args           Unused
-     * @exception Exception  Throws exception if anything goes wrong
-     */
-	 public static void main(String[] args) {
-		 Palindrome palindrome = new Palindrome();
-		 String output = palindrome.findNearest("-123-?123");
-		 System.out.println(output);
-	 }
-	
-    /**
-     * Loops through the characters within the string forwards and backwards consecutively
-     * to determine whether the string is a valid palindrome.
-     * @param string    A string of integers.
-     * @return boolean  Returns true if the string argument is a palindrome otherwise it returns false.
-     */
-    public boolean isValid(String string) {
-        int i = 0;
-        int j = string.length() - 1;
+	 * Main method that creates a new instance of the Palindrome class and calls the
+	 * main method to find the nearest palindrome from a given string of integers
+	 * 
+	 * @param args Unused
+	 * @exception Exception Throws exception if anything goes wrong
+	 */
+	public static void main(String[] args) {
+		Palindrome palindrome = new Palindrome();
+		String output = palindrome.findNearest("-123-?123");
+		System.out.println(output);
+	}
 
-        while (i < j) {
-            if (string.charAt(i) != string.charAt(j)) {
-                return false;
-            }
+	/**
+	 * Loops through the characters within the string forwards and backwards
+	 * consecutively to determine whether the string is a valid palindrome.
+	 * 
+	 * @param string A string of integers.
+	 * @return boolean Returns true if the string argument is a palindrome otherwise
+	 *         it returns false.
+	 */
+	public boolean isValid(String string) {
+		int i = 0;
+		int j = string.length() - 1;
 
-            i++;
-            j--;
-        }
-        return true;
-    }
+		while (i < j) {
+			if (string.charAt(i) != string.charAt(j)) {
+				return false;
+			}
 
-    /**
-     * Converts the string argument to a valid palindrome.
-     * @param string   A string of integers.
-     * @return String  A valid palindrome.
-     */
-    public String convert(String string) {
-        if (isValid(string)) {
-            return string;
-        }
-    
-        int strLen = string.length();
-        int halfIndex = strLen / 2;
-        boolean isOdd = strLen % 2 == 1;
-        String palindrome = "";
-    
-        for (int i = 0; i < halfIndex; i++) {
-            palindrome += string.charAt(i);
-        }
+			i++;
+			j--;
+		}
+		return true;
+	}
 
-        if (isOdd) {
-            palindrome += string.charAt(halfIndex);
-        }
+	/**
+	 * Converts the string argument to a valid palindrome.
+	 * 
+	 * @param string A string of integers.
+	 * @return String A valid palindrome.
+	 */
+	public String convert(String string) {
+		if (isValid(string)) {
+			return string;
+		}
 
-        for (int i = halfIndex - 1; i >= 0; i--) {
-            palindrome += string.charAt(i);
-        }
-    
-        return palindrome;
-    }
+		int strLen = string.length();
+		int halfIndex = strLen / 2;
+		boolean isOdd = strLen % 2 == 1;
+		String palindrome = "";
 
-    /**
-     * Converts the original string argument to a palindrome, creates two (preceding and succeeding) palindrome sequences 
-     * and compares the difference between them all with the lowest being returned.
-     * @param string   A string of integers
-     * @return String  The nearest palindrome. If the difference is equal between any of palindrome sequences then it
-     *                 will return the lower one.
-     */
-    public String findNearest(String string) {
-        if (string == null || string.isEmpty() || string.trim().isEmpty()) {
-            return "Please enter a number";
-        }
-        
-        if (!string.matches("-?[0-9]+")) {
-            return "Please enter a valid number";
-        }
+		for (int i = 0; i < halfIndex; i++) {
+			palindrome += string.charAt(i);
+		}
 
-        int length = string.length();
-        int number = Integer.parseInt(string);
+		if (isOdd) {
+			palindrome += string.charAt(halfIndex);
+		}
 
-        if (number < 0) {
-            return "Negative numbers cannot be a palindrome";
-        }
+		for (int i = halfIndex - 1; i >= 0; i--) {
+			palindrome += string.charAt(i);
+		}
 
-        if (number == 0) {
-            return "Cannot return a palindrome that is less than zero";
-        }
+		return palindrome;
+	}
 
-        if (number > 0 && length < 2) {
-            return Integer.toString(number - 1);
-        }
+	/**
+	 * Converts the original string argument to a palindrome, creates two (preceding
+	 * and succeeding) palindrome sequences and compares the difference between them
+	 * all with the lowest being returned.
+	 * 
+	 * @param string A string of integers
+	 * @return String The nearest palindrome. If the difference is equal between any
+	 *         of palindrome sequences then it will return the lower one.
+	 */
+	public String findNearest(String string) {
+		if (string == null || string.isEmpty() || string.trim().isEmpty()) {
+			return "Please enter a number";
+		}
 
-        int power = length / 2;
-        int calcPow = (int) Math.pow(10, power);
-        String low = Integer.toString(number - calcPow);
-        String reg = string;
-        String high = Integer.toString(number + calcPow);
+		if (!string.matches("-?[0-9]+")) {
+			return "Please enter a valid number";
+		}
 
-        if (length % 2 == 0 && low.length() != reg.length()) {
-            low = "9".repeat(low.length());
-        }
+		int length = string.length();
+		int number = Integer.parseInt(string);
 
-        String lowerBound = convert(low);
-        String regBound = convert(reg);
-        String upperBound = convert(high);
+		if (number < 0) {
+			return "Negative numbers cannot be a palindrome";
+		}
 
-        ArrayList<String> boundaries = new ArrayList<String>();
-        boundaries.add(lowerBound);
-        if (!isValid(reg)) {
-            boundaries.add(regBound);
-        }
-        boundaries.add(upperBound);
+		if (number == 0) {
+			return "Cannot return a palindrome that is less than zero";
+		}
 
-        int index = 0;
-        int value = Math.abs(number - Integer.parseInt(boundaries.get(0)));
-        for (int i = 0; i < boundaries.size(); i++) {
-            int diff = Math.abs(number - Integer.parseInt(boundaries.get(i)));
-            if (diff < value) {
-            	value = diff;
-                index = i;
-            }
-        }
-        return boundaries.get(index); 
-    }
+		if (number > 0 && length < 2) {
+			return Integer.toString(number - 1);
+		}
+
+		int power = length / 2;
+		int calcPow = (int) Math.pow(10, power);
+		String low = Integer.toString(number - calcPow);
+		String reg = string;
+		String high = Integer.toString(number + calcPow);
+
+		if (length % 2 == 0 && low.length() != reg.length()) {
+			low = "9".repeat(low.length());
+		}
+
+		String lowerBound = convert(low);
+		String regBound = convert(reg);
+		String upperBound = convert(high);
+
+		ArrayList<String> boundaries = new ArrayList<String>();
+		boundaries.add(lowerBound);
+		if (!isValid(reg)) {
+			boundaries.add(regBound);
+		}
+		boundaries.add(upperBound);
+
+		int index = 0;
+		int value = Math.abs(number - Integer.parseInt(boundaries.get(0)));
+		for (int i = 0; i < boundaries.size(); i++) {
+			int diff = Math.abs(number - Integer.parseInt(boundaries.get(i)));
+			if (diff < value) {
+				value = diff;
+				index = i;
+			}
+		}
+		return boundaries.get(index);
+	}
 }
--- src/tests/PalindromeTest.java
+++ src/tests/PalindromeTest.java
@@ -1,6 +1,6 @@
 /**
  * 
- * The PalindromeTest class tests the majority of the use cases that I've come accross
+ * The PalindromeTest class tests the majority of the use cases that I've come across
  * 
  * @author Jason Cheong
  * @version 1.0
@@ -8,111 +8,111 @@
  */
 package tests;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import app.Palindrome;
 
-import static org.junit.Assert.*;
-
 public class PalindromeTest {
 	private Palindrome palindrome;
 	private String input;
 	private String output;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		input = null;
 		palindrome = new Palindrome();
 	}
-	
+
 	@After
 	public void tearDown() throws Exception {
-		
+
 	}
-	
+
 	@Test
-    public void nullStringTest() throws Exception {
-        input = null;
-        output = palindrome.findNearest(input);
-        assertEquals("Please enter a number", output);
-    }
-	
+	public void nullStringTest() throws Exception {
+		input = null;
+		output = palindrome.findNearest(input);
+		assertEquals("Please enter a number", output);
+	}
+
 	@Test
-    public void emptyStringTest() throws Exception {
-        input = "";
-        output = palindrome.findNearest(input);
-        assertEquals("Please enter a number", output);
-    }
-	
+	public void emptyStringTest() throws Exception {
+		input = "";
+		output = palindrome.findNearest(input);
+		assertEquals("Please enter a number", output);
+	}
+
 	@Test
-    public void whiteSpaceTest() throws Exception {
+	public void whiteSpaceTest() throws Exception {
 		input = " ";
-        output = palindrome.findNearest(input);
-        assertEquals("Please enter a number", output);
-    }
-	
+		output = palindrome.findNearest(input);
+		assertEquals("Please enter a number", output);
+	}
+
 	@Test
-    public void alphabetTest() throws Exception {
+	public void alphabetTest() throws Exception {
 		input = "abc";
-        output = palindrome.findNearest(input);
-        assertEquals("Please enter a valid number", output);
-    }
-	
+		output = palindrome.findNearest(input);
+		assertEquals("Please enter a valid number", output);
+	}
+
 	@Test
-    public void numberFormatTest() throws Exception {
+	public void numberFormatTest() throws Exception {
 		input = "-123-?456";
-        output = palindrome.findNearest(input);
-        assertEquals("Please enter a valid number", output);
-    }
-	
+		output = palindrome.findNearest(input);
+		assertEquals("Please enter a valid number", output);
+	}
+
 	@Test
-    public void numberIsNegativeTest() throws Exception {
+	public void numberIsNegativeTest() throws Exception {
 		input = "-12";
-        output = palindrome.findNearest(input);
-        assertEquals("Negative numbers cannot be a palindrome", output);
-    }
-	
+		output = palindrome.findNearest(input);
+		assertEquals("Negative numbers cannot be a palindrome", output);
+	}
+
 	@Test
-    public void numberIsZeroTest() throws Exception {
+	public void numberIsZeroTest() throws Exception {
 		input = "0";
-        output = palindrome.findNearest(input);
-        assertEquals("Cannot return a palindrome that is less than zero", output);
-    }
-	
+		output = palindrome.findNearest(input);
+		assertEquals("Cannot return a palindrome that is less than zero", output);
+	}
+
 	@Test
-    public void singleDigitTest() throws Exception {
+	public void singleDigitTest() throws Exception {
 		input = "8";
-        output = palindrome.findNearest(input);
-        assertEquals("7", output);
-    }
-	
+		output = palindrome.findNearest(input);
+		assertEquals("7", output);
+	}
+
 	@Test
-    public void doubleDigitTest() throws Exception {
+	public void doubleDigitTest() throws Exception {
 		input = "77";
-        output = palindrome.findNearest(input);
-        assertEquals("66", output);
-    }
-	
+		output = palindrome.findNearest(input);
+		assertEquals("66", output);
+	}
+
 	@Test
-    public void oddStringLengthTest() throws Exception {
+	public void oddStringLengthTest() throws Exception {
 		input = "10001";
-        output = palindrome.findNearest(input);
-        assertEquals("9999", output);
-    }
-	
+		output = palindrome.findNearest(input);
+		assertEquals("9999", output);
+	}
+
 	@Test
-    public void evenStringLengthTest() throws Exception {
+	public void evenStringLengthTest() throws Exception {
 		input = "1001";
-        output = palindrome.findNearest(input);
-        assertEquals("999", output);
-    }
-	
+		output = palindrome.findNearest(input);
+		assertEquals("999", output);
+	}
+
 	@Test
-    public void tiedDifferenceTest() throws Exception {
+	public void tiedDifferenceTest() throws Exception {
 		input = "1056";
-        output = palindrome.findNearest(input);
-        assertEquals("1001", output);
-    }
+		output = palindrome.findNearest(input);
+		assertEquals("1001", output);
+	}
 }
